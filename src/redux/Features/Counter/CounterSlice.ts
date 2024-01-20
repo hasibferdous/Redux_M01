b0557ba02e62ea8a903d03a2e2e6eabd2634f795
hasibferdous <hasibferdous.ict@gmail.com>
2024-01-21 02:07:19 +0600

    connecting components with redux store

---
+++ src/redux/Features/Counter/CounterSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    count: 0,
+  }
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+      increment: (state) => {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the Immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        state.count = state.count+1
+      },
+      decrement: (state) => {
+        state.count = state.count-1
+      },
+    },
+  })
+
+  export default counterSlice.reducer;
--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,6 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import counterReducer from "./Features/Counter/CounterSlice"
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 })
 export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
