1e8473eb3132524f6e5d820acf9e579ff5b4c804
hasibferdous <hasibferdous.ict@gmail.com>
2024-01-21 01:25:48 +0600

    set up redux store and connecting redux with react done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
-import Home from "../page/Home";
 import App from "../App";
 import Login from "../page/Login";
+import NotFound from "../page/NotFound";
   
   const routes = createBrowserRouter([
     {
@@ -9,8 +9,8 @@ import Login from "../page/Login";
       element: <App/>,
     },
     {
-        path: "/home",
-        element: <Home/>,
+        path: "*",
+        element: <NotFound/>,
     },
     {
         path: "/login",
